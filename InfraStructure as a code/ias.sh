2898b734a76fbcc4aa85897d9aa542d0127b78a1
lrnas <lr.nas1894@gmail.com>
2022-11-17 17:44:26 +0000

    Atualizando código

--- InfraStructure as a code/ias.sh
+++ InfraStructure as a code/ias.sh
@@ -2,40 +2,76 @@
 
 cd /home
 
-rm -Rf ./guest1
-userdel -r guest1
-
+echo "Criando listas"
 # Lista de pastas e permissões
-declare -a pastas = ('./publico' './adm' './ven' './sec')
-declare -a permit = (777 770 770 770)
+declare -a pastas=('./publico' './adm' './ven' './sec')
+declare -a permit=(777 770 770 770)
 
 # Lista de usuários 
 declare -a users=('carlos' 'maria' 'joao' 'debora' 'sebastiana' 'roberto' 'josefina' 'amanda' 'rogerio')
-declare -a usergroups = ( 0 0 0 1 1 1 2 2 2)
-declare -a usercomment = ('Carlos da ADM' 'Maria da ADM' 'João da ADM' 'Débora de Vendas' 'Sebastiana de Vendas' 'Roberto de Vendas' 'Josefina de Secretariado' 'Amanda de Secretariado' 'Rogério de Secretariado')
+declare -a usergroups=(0 0 0 1 1 1 2 2 2)
+declare -a usercomment=('Carlos da ADM' 'Maria da ADM' 'João da ADM' 'Débora de Vendas' 'Sebastiana de Vendas' 'Roberto de Vendas' 'Josefina de Secretariado' 'Amanda de Secretariado' 'Rogério de Secretariado')
 
 # Lista de grupos
 declare -a groups=('grp_adm' 'grp_ven' 'grp_sec')
 
+criar_infraestrutura(){
+echo "Criando infraestrutura de dados"
 for i in "${groups[@]}"
 do
-   groupadd $i
+	groupadd $i
 done
 
-$tamanho=${#pastas[@]}
+declare -i tamanho=${#pastas[@]}
 
-for (( j=0; j<${tamanho}; j++ ));
+for ((j=0;j<${tamanho};j++))
 do
-  mkdir ${pastas[$j]}
-  chmod =${permit[$j]} ${pastas[$j]} -R
-  if($j!=0)
-  then
-    chown root:${groups[$j-1]} ${pastas[$j]} -R
-  fi
+  	mkdir ${pastas[$j]}
+  	chmod =${permit[$j]} ${pastas[$j]} -R
+	if(($j>0)); then
+    		chown root:${groups[$j-1]} ${pastas[$j]} -R
+  	fi
+done
+tamanho=${#users[@]}
+for ((j=0;j<${tamanho};j++))
+	do
+	useradd ${users[$j]} -c "${usercomment[$j]}"  -s /bin/bash -m -p $(openssl passwd Senha1234)
+	usermod ${users[$j]} -G ${usergroups[$j]} -a
+done
+echo "Infraestrutura de dados criada"
+}
+
+
+deletar_anterior(){
+# Apagar dados anteriores
+echo "Apagando infraestrutura de dados anterior"
+for i in "${users[@]}"
+do
+	rm -Rf "./$i"
+	userdel -rf $i
 done
 
-$tamanho=${#users[@]}
-for (( j=0; j<${tamanho}; j++ ));
+for i in "${pastas[@]}"
 do
-    useradd ${users[$j]} -c ${usercomment[$j]}  -s /bin/bash -m -p $(openssl passwd Senha1234) -a ${usergroups[$j]} 
+	rm -Rf $i
 done
+
+for i in "${groups[@]}"
+do
+	groupdel -f $i
+done
+echo "Infraestrutura de dados anterior apagada"
+}
+
+
+echo "Listas criadas."
+#echo "Digite sua ação:"
+#echo "1 - Apagar infraestrutura de dados anterior"
+#echo "2 - Criar infraestrutura de dados"
+#echo "3 - Sair"
+#read var1
+
+deletar_anterior
+read var1
+criar_infraestrutura
+read var1
